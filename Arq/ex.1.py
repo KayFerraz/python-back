fa89f795997524c677bd910d16dff91c13bba8d6
Nome Temporário <kayannesouza14@gmail.com>
2025-10-17 10:49:33 -0300

    arq

---
+++ Arq/ex.1.py
@@ -0,0 +1,9 @@
+try: 
+    arq = open("C:..\\Dev-Cpp\\NEWS.txt","r")
+    arq.close()
+except:
+    print("ERRO DE ABERTURA")
+
+    
+    
+
---
+++ Aula3/exemplo.py
@@ -0,0 +1,7 @@
+def poligono (*base):
+    a = input(int("Informe a base"))
+    base = 2 * a
+    print(base)
+  
+
+poligono(*2)
