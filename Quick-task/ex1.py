4addef2f1bb4719b89227cdd9f245f31ef154481
Kay Ferraz <kayannesouza14@gmail.com>
2025-10-22 23:44:29 -0300

    ex1 quick task

---
+++ Quick-task/ex1.py
@@ -0,0 +1,22 @@
+#  Utilizando o DB Browser, crie uma base de dados com uma 
+# tabela para armazenar os dados 
+# dos colaboradores ou dos idosos seu projeto da disciplina.
+#  Não esqueça de considerar que 
+# cada idoso/colaborador tem um ID
+import sqlite3
+try:
+    iniciar = sqlite3.connect("voluntarios.db")
+    cur = iniciar.cursor()
+    nome='Maria Antonia'
+    voluntario = 'NAO'
+    sql=f"INSERT INTO colaborador VALUES (589696564452,'LUA', 'SIM')"
+    cur.execute(sql)
+    iniciar.commit()
+    sql="SELECT nome FROM colaborador"
+    cur.execute(sql)
+    res=cur.fetchall()
+    print(res)
+    cur.close()
+    iniciar.close()
+except sqlite3.Error as error:
+    print(error)
---
+++ Quick-task/ex2.py
@@ -0,0 +1,6 @@
+# . Crie a função inserir que recebe como parâmetros um
+#  dicionário com os dados de um 
+# idosos/colaboradores e usando o
+#  comando SQL INSERT, armazene os dados na tabela
+
+import sqlite3
